acd38df843d6dbbe9e6b02cc32a4bfd9fcfa14a9
Tim <mideltim@gmail.com>
2024-07-08 21:52:31 +0800

    show more book information through dialog modal when clicking a book

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
-const library = [];
+//Initialize few books present in the library
+const library = [
+  {
+    title: "The Lord of the Rings",
+    author: "The Author",
+    dateOfPublication: "2002-08-04",
+    numberOfPages: 200,
+    haveRead: false,
+  },
+  {
+    title: "The Sea of Monsters",
+    author: "The Author is an Author",
+    dateOfPublication: "2002-08-04",
+    numberOfPages: 200,
+    haveRead: false,
+  },
+  {
+    title: "The Great Gatsby",
+    author: "The Author",
+    dateOfPublication: "2002-08-04",
+    numberOfPages: 200,
+    haveRead: false,
+  },
+  {
+    title: "The Lord of the Rings",
+    author: "The Author",
+    dateOfPublication: "2002-08-04",
+    numberOfPages: 200,
+    haveRead: false,
+  },
+  {
+    title: "The Sea of Monsters",
+    author: "The Author",
+    dateOfPublication: "2002-08-04",
+    numberOfPages: 200,
+    haveRead: false,
+  },
+];
 
-function Book(title, author, dateOfPublication, numberOfPages, haveRead) {
-  this.title = title;
-  this.author = author;
-  this.dateOfPublication = dateOfPublication;
-  this.numberOfPages = numberOfPages;
-  this.haveRead = haveRead;
+class Book {
+  constructor(title, author, dateOfPublication, numberOfPages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.dateOfPublication = dateOfPublication;
+    this.numberOfPages = numberOfPages;
+    this.haveRead = haveRead;
+  }
 }
 
-//Add book
+const addBookForm = document.querySelector("#add-book-form");
+const addBookButton = document.querySelector("#add-book");
+const closeAddBook = document.querySelector("#add-book-form button");
+
+const bookInfo = document.querySelector("#book-info");
+const removeBookButton = document.querySelector("#remove-book");
+const closeBookInfo = document.querySelector("#book-info button");
+
+const bookContainers = document.querySelectorAll(".book-container");
+
+//Show books available in library
+document.addEventListener("DOMContentLoaded", function () {
+  showBooks();
+});
+
+//Add book to the library
 document.getElementById("book-form").addEventListener("submit", (e) => {
   const form = e.target;
   // Get the input values
@@ -21,25 +75,89 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
   library.push(book);
   form.reset();
   console.log(book);
+  showBooks();
 });
 
 const showBooks = () => {
-  const book = document.createElement("div");
-  book.style.cssText = "color: blue; background: white;";
-  book.classList.add("book");
-  book.textContent("New Book");
+  // Clear existing book elements
+
+  bookContainers.forEach((container) => {
+    container.innerHTML = "";
+  });
+
+  library.forEach((bookElement, index) => {
+    const books = document.querySelectorAll(".book");
+
+    const book = document.createElement("div");
+    book.classList.add("book");
+    const title = document.createElement("h4");
+    title.textContent = `${bookElement.title}`;
+    const author = document.createElement("h5");
+    author.textContent = `${bookElement.author}`;
+    const dateOfPublication = document.createElement("p");
+    dateOfPublication.textContent = `${bookElement.dateOfPublication}`;
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(dateOfPublication);
+
+    book.setAttribute("data-index", index);
+
+    let shelfIndex = 0;
+    if (books.length > 11) {
+      shelfIndex = 2;
+    } else if (books.length > 5) {
+      shelfIndex = 1;
+    }
+    console.log(books.length, shelfIndex);
+    bookContainers[shelfIndex].appendChild(book);
+  });
 };
 
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#add-book");
-const closeButton = document.querySelector("dialog button");
-
 // "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-  dialog.showModal();
+addBookButton.addEventListener("click", () => {
+  const books = document.querySelectorAll(".book");
+  if (books.length > 18) {
+    console.log("The library is full");
+  }
+  addBookForm.showModal();
 });
 
 // "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-  dialog.close();
+closeAddBook.addEventListener("click", () => {
+  addBookForm.close();
+});
+
+const openBookInfo = (event) => {
+  // Check if the clicked element is inside a .book element
+  const bookElement = event.target.closest(".book");
+  if (bookElement) {
+    const dataIndex = bookElement.getAttribute("data-index");
+    const currentBook = library[dataIndex];
+
+    const title = document.querySelector("#book-content h3");
+    title.textContent = `Title: ${currentBook.title}`;
+
+    const author = document.querySelector("#book-content h4");
+    author.textContent = `Author: ${currentBook.author}`;
+
+    const paragraphs = document.querySelectorAll("#book-content p");
+
+    const dateOfPublication = paragraphs[0];
+    dateOfPublication.textContent = `Date of Publication: ${currentBook.dateOfPublication}`;
+
+    const numberOfPages = paragraphs[1];
+    numberOfPages.textContent = `Number of Pages: ${currentBook.numberOfPages}`;
+
+    bookInfo.showModal();
+  }
+};
+
+bookContainers.forEach((container) => {
+  // Attach event listener to each child with class "book"
+  container.addEventListener("click", openBookInfo);
+});
+
+// "Close" button closes the dialog
+closeBookInfo.addEventListener("click", () => {
+  bookInfo.close();
 });
